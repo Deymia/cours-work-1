ce12fcef53134d4f9507e34dc2e5983e67218f04
Anastasia <shvayka92@mail.ru>
2022-07-11 17:40:32 +0300

    first

---
+++ src/Employee.java
@@ -0,0 +1,67 @@
+public class Employee {
+    private String employeeName;
+    private int department;
+    private float salary;
+    private static int counter = 1;
+    private int id;
+
+
+    public Employee(String employeeName, int department, float salary) {
+        this.employeeName = employeeName;
+        this.department = department;
+        this.salary = salary;
+        this.id = getCounter();
+        counter++;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    @Override
+    public String toString() {
+        return id+ " ФИО сотрудника: " + this.employeeName + "\n Номер отдела: " + this.department + "\n Заработная плата: " + this.salary + "\n";
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+
+/*    public static float countSalary(float salary) {
+
+        float sum = 0;
+        sum = sum + salary;
+
+        return sum;
+
+    }*/
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
+
+/*    public static float countSalary (float salary){
+
+        float sum = 0;
+        for (int i = 0; i < )
+        sum = sum + salary;
+
+        return sum;
+    }*/
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee [] employees = new Employee[10];
+            employees[0] = new Employee("Иванов Сергей Сергеевич", 1, 120_000);
+            employees[1] = new Employee("Петров Иван Иванович", 2, 105_000);
+            employees[2] = new Employee("Кузнецова Мария Ивановна", 2, 110_000);
+            employees[3] = new Employee("Семенова Ирина Владимировна", 3, 61_000);
+            employees[4] = new Employee("Круглов Петр Петрович", 4, 54_000);
+            employees[5] = new Employee("Сергеева Анна Ивановна", 5, 132_000);
+            employees[6] = new Employee("Лукичев Олег Сергеевич", 4, 124_000);
+            employees[7] = new Employee("Радионов Семен Васильевич", 1,45_000);
+            employees[8] = new Employee("Павлова Марина Анатольевна", 3, 150_000);
+            employees[9] = new Employee("Алексеева Маргарита Влладимировна", 4, 44_000);
+
+
+        System.out.println("Количество сотрудников: " + Employee.getCounter());
+        System.out.println(Arrays.toString(employees));
+
     }
 }
